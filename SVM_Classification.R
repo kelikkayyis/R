1806f102bb061cc27b9f3006d2c178a5046f352e
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2021-07-21 12:05:23 +0700

    Update Fromat
    Update Fromat & Keterangan

--- SVM_Classification.R
+++ SVM_Classification.R
@@ -1,28 +1,35 @@
-rm(list=ls())
-
+# Set working directoris
 setwd("~/_R_Stats/Mastering R - SA/PR3")
+
+# Import Data
 datafh <- read.csv("fetal_health.csv")
 
+# Explorasi Data
 dim(datafh)
 str(datafh)
 names(datafh)
 summary(datafh)
-sum(is.na(datafh))
+colSums(is.na(datafh))
 
+# Memisahkan variabel x dan variabel y
 x <- datafh[,-22]
 names(x)
 y <- datafh[22]
 names(y)
 
+# Merubah format kolom fetal_health menjadi factor agak terbaca sebagai labels
 y$fetal_health <- as.factor(y$fetal_health)
 str(y)
 
+# Membuat model SVM Klasifikasi
 library(e1071)
 model_fh <- svm(fetal_health~., data = datafh, type = "C-classification")
 summary(model_fh)
 
+# Membuat Prediksi dengan data awal
 pred_fh <- predict(model_fh,x)
 summary(pred_fh)
 
+# Membuat Confution Matrix
 library(caret)
 confusionMatrix(pred_fh,y$fetal_health)
